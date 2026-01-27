89da5596c040ba079898d8490d292743e02d2883
Italo Bello <italo.bello10@gmail.com>
2026-01-27 15:01:06 -0300

    initial commit

---
+++ backend/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
+	fmt.Println("Server running on port 8080")
+	r.Run(":8080")
+}
